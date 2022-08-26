9ad83d7a882a817c4c378d06ad76353baf7e9b6e
MohdAnas07 <anas98144@gmail.com>
2022-08-26 16:13:42 +0530

    share component ready

--- src/components/feed/Feed.jsx
+++ src/components/feed/Feed.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import Share from '../share/Share'
 import './feed.css'
 
 export default function Feed() {
     return (
-        <div className='feed'>Feed Component</div>
+        <div className='feed'>
+            <div className="feedWrapper">
+                <Share />
+            </div>
+        </div>
     )
 }
