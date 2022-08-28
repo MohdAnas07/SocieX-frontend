0585b5284f0dbdde1c97229842ab4a1c128a8140
MohdAnas07 <anas98144@gmail.com>
2022-08-28 11:52:23 +0530

    frontend for SocieX almost Done

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import './topbar.css'
+import { Link } from 'react-router-dom';
 import SearchIcon from '@mui/icons-material/Search';
 import PersonIcon from '@mui/icons-material/Person'
 import ChatIcon from '@mui/icons-material/Chat';
@@ -9,7 +10,7 @@ export default function Topbar() {
     return (
         <div className="topbarContainer">
             <div className="topbarLeft">
-                <div className="logo">SocieX </div>
+                <Link to='/home'> <span className="logo">SocieX </span></Link>
             </div>
             <div className="topbarCenter">
                 <div className="searchbar">
@@ -36,7 +37,7 @@ export default function Topbar() {
                         <span className="topbarIconBadge">1</span>
                     </div>
                 </div>
-                <img src="/assets/person/1.jpeg" alt="" className="topbarImg" />
+                <Link to='/profile'><img src="/assets/person/1.jpeg" alt="" className="topbarImg" /></Link>
             </div>
         </div>
     )
